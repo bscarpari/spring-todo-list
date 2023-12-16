7831a8b3cf3f8c7601976f4d11be8ba925bff2c1
Scarpari <brunoomoraes686@gmail.com>
2023-12-16 15:26:17 -0300

    add: initial HTTP request

--- src/main/java/br/com/bscarpari/todolist/TodolistApplication.java
+++ src/main/java/br/com/bscarpari/todolist/TodolistApplication.java
@@ -3,8 +3,6 @@ package br.com.bscarpari.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.bscarpari.todolist.controller.FirstController;
-
 // @algumNome -> annotation (função) que vai ser executada pelo Spring
 @SpringBootApplication
 public class TodolistApplication {
@@ -12,10 +10,6 @@ public class TodolistApplication {
 	// Método main para executar a aplicação (principal)
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-
-		FirstController firstController = new FirstController();
-
-		firstController.hello();
 	}
 
 }
